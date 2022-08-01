2dd4f10834c8a73c0bea904f5130cd68d532dee2
alextech303 <alextech303@gmail.com>
2022-08-01 10:57:27 +0300

    first commit

---
+++ src/main/java/web_project_14/App.java
@@ -0,0 +1,13 @@
+package web_project_14;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
